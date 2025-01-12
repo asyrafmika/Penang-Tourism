76f4e292f8a3e4561c4156f8d1dd2bc4deea65cc
Asyrafmika <ahmadasyraf30181@gmail.com>
2025-01-12 22:02:02 +0800

    buat kali kelima aku tukar link

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -14,7 +14,7 @@ const VideoPlayer = ({playState, setPlayState}) => {
   return (
     <div className={`video-player ${playState? '' : 'hide'}`} 
     ref={player} onClick={closePlayer}>
-        <video src='https://drive.google.com/file/d/1rYtRTXrG9xQQP6SpC78LPAcK_vtClJWd/view?usp=sharing' autoPlay muted controls></video>
+        <video url='https://drive.google.com/file/d/1rYtRTXrG9xQQP6SpC78LPAcK_vtClJWd/view?usp=sharing' autoPlay muted controls></video>
     </div>
   )
 }
