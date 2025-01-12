95a0619f972c6e361a5b70b6e2207dc601b8a538
asyrafmika <167151750+asyrafmika@users.noreply.github.com>
2025-01-13 02:32:51 +0800

    Update Shopping.jsx

--- src/Pages/Food/Shopping.jsx
+++ src/Pages/Food/Shopping.jsx
@@ -1,54 +1,62 @@
 // Import React and necessary assets
 import React, { useState } from 'react';
 import './Shopping.css';
+import gurneyplaza from "../../assets/spas/gurneyplaza.png";
+import queensbay from "../../assets/spas/queensbay.png";
+import avenuemall from "../../assets/spas/avenuemall.png";
+import nightmarket from "../../assets/spas/nightmarket.png";
+import rockspa from "../../assets/spas/rockspa.png";
+import chispa from "../../assets/spas/chispa.png";
+import towerspa from "../../assets/spas/towerspa.png";
+import penagaspa from "../../assets/spas/penagaspa.png";
 
 // Sample content for shopping (this can later come from a separate file or API)
 const shoppingPlaces = [
   {
     name: "Gurney Plaza",
-    image: "src/assets/spas/gurneyplaza.png",
+    image: gurneyplaza,
     description: "It is Penang’s premier lifestyle shopping mall and a one-stop shopping and entertainment destination catering to both families and tourists arriving in Gurney Drive, Penang.",
     address: "170, Gurney Dr, Pulau Tikus, 10250 George Town, Penang, Malaysia",
   },
   {
     name: "Queensbay Mall",
-    image: "src/assets/spas/queensbay.png",
+    image: queensbay,
     description: "It is currently the largest, longest and most modern shopping mall in Penang. Located in the heart of Bayan Lepas, it is the most preferred shopping destination for both the locals and tourists.",
     address: "100, Persiaran Bayan Indah, 11900 Bayan Lepas, Pulau Pinang, Malaysia",
   },
   {
     name: "1st Avenue Mall",
-    image: "src/assets/spas/avenuemall.png",
+    image: avenuemall,
     description: "A stylish city mall located in the heart of Georgetown for urbanites and young executives, with trendy high-street fashion, dining, and entertainment.",
     address: "182, Jalan Magazine, 10300 George Town, Penang, Malaysia",
   },
   {
     name: "Penang Night Markets",
-    image: "src/assets/spas/nightmarket.png",
+    image: nightmarket,
     description: "Night markets in Penang are known for their lively atmosphere that truly depicts Malaysian culture, while offering local crafts, souvenirs, and delicious street food.",
     address: "Penang, Malaysia",
   },
   {
     name: "Rock Spa @ Hard Rock Penang",
-    image: "src/assets/spas/rockspa.png",
+    image: rockspa,
     description: "Rock Spa offers a diverse selection of spa treatments with a full host of amenities, you will be amazed by a “spa-tastic” experience that fit the Rock Star in you and for the lil’ rockers’ too!",
     address: "Batu Ferringhi Beach, 11100 Penang, Malaysia",
   },
   {
     name: "Chi, The Spa @ Shangri-La Rasa Sayang",
-    image: "src/assets/spas/chispa.png",
+    image: chispa,
     description: "The Spa is equipped with a private changing room, a treatment room and a private garden. The spa menu includes an assortment of healing and relaxing Chinese and Malay treatments.",
     address: "Batu Ferringhi Beach, 11100 Penang, Malaysia",
   },
   {
     name: "Tower Spa @ The TOP Komtar",
-    image: "src/assets/spas/towerspa.png",
+    image: towerspa,
     description: "Tower Spa is a whole new world designed for you to escape from the busy city lifestyle and enjoy some quiet me time, or even for a short “honeymoon” with your loved ones.",
     address: "1, Jalan Penang, George Town, 10000 George Town, Pulau Pinang, Malaysia",
   },
   {
     name: "Penaga Spa @ Hotel Penaga Penang",
-    image: "src/assets/spas/penagaspa.png",
+    image: penagaspa,
     description: "One of the best spa in Penang, offering an unmatched healing experience. The spa's treatments, infused with the essence of the penaga tree, create a serene haven for holistic relaxation and inner balance.",
     address: "Corner of Jalan Hutton & Lebuh Clarke, George Town, Penang, Malaysia",
   },
